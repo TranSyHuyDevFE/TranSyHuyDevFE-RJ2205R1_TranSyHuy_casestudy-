55268d828056bffac93b57d5f06ea20d52531634
TranSyHuy <syhuy01072002@gmail.com>
2022-08-25 22:39:25 +0700

    first commit

--- src/pages/NavBar.js
+++ src/pages/NavBar.js
@@ -1,26 +1,8 @@
 import React from "react";
-import Swal from "sweetalert2";
+
 import { useSelector } from "react-redux";
-import withReactContent from "sweetalert2-react-content";
 
 export default function NavBar() {
-  const alert = () => {
-    const MySwal = withReactContent(Swal);
-    MySwal.fire({
-      title: "Do you want to quit ?",
-      text: "You will log out and result won't save !",
-      showDenyButton: true,
-      confirmButtonText: "Continue",
-      denyButtonText: `Quit`,
-    }).then((result) => {
-      /* Read more about isConfirmed, isDenied below */
-      if (result.isConfirmed) {
-        Swal.fire("Go on!", "", "success");
-      } else if (result.isDenied) {
-        window.location.href = "/";
-      }
-    });
-  };
   const userLogged_in = useSelector((state) => state.loginState.userLogged_in);
   return (
     <div className="flex justify-between items-center w-full h-20 text-white fixed  bg-gray-900  px-4">
@@ -36,10 +18,7 @@ export default function NavBar() {
             Hi,&nbsp;{userLogged_in.username}
           </h3>
         )}
-        <button
-          onClick={alert}
-          className="shadow-sm bg-red-900 rounded-lg px-4 ml-2 hover:shadow-red-50/50"
-        >
+        <button className="shadow-sm bg-red-900 rounded-lg px-4 ml-2 hover:shadow-red-50/50">
           {userLogged_in.username === undefined ? null : "Quit"}
         </button>
       </ul>
