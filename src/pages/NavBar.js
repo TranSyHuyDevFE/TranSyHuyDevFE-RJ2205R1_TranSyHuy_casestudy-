48c73ddf136810ce0004ff044996f0a39182fc2c
TranSyHuy <syhuy01072002@gmail.com>
2022-08-25 22:55:08 +0700

    first commit

--- src/pages/NavBar.js
+++ src/pages/NavBar.js
@@ -1,8 +1,26 @@
 import React from "react";
+import swal from "sweetalert";
 
 import { useSelector } from "react-redux";
 
 export default function NavBar() {
+  const alert = () => {
+    swal({
+      title: "Are you sure?",
+      text: "You will log out && result won't save!",
+      icon: "warning",
+      buttons: true,
+      dangerMode: true,
+    }).then((willDelete) => {
+      if (willDelete) {
+        window.location.href = "/";
+      } else {
+        swal("Go on!", {
+          icon: "success",
+        });
+      }
+    });
+  };
   const userLogged_in = useSelector((state) => state.loginState.userLogged_in);
   return (
     <div className="flex justify-between items-center w-full h-20 text-white fixed  bg-gray-900  px-4">
@@ -18,7 +36,10 @@ export default function NavBar() {
             Hi,&nbsp;{userLogged_in.username}
           </h3>
         )}
-        <button className="shadow-sm bg-red-900 rounded-lg px-4 ml-2 hover:shadow-red-50/50">
+        <button
+          onClick={alert}
+          className="shadow-sm bg-red-900 rounded-lg px-4 ml-2 hover:shadow-red-50/50"
+        >
           {userLogged_in.username === undefined ? null : "Quit"}
         </button>
       </ul>
--- src/store/saga/login.js
+++ src/store/saga/login.js
@@ -1,12 +1,22 @@
 import { put, takeLatest } from "redux-saga/effects";
 import * as stages from "../../utils/constants";
 import { loginGame } from "../slices/loginInit";
-
+import swal from "sweetalert";
 function* authSagaFun(action) {
   const userLogged_in = action.payload;
   if (userLogged_in.username === "syhuy" && userLogged_in.password === "123") {
+    swal("Hello!", {
+      buttons: false,
+      icon: "success",
+      timer: 2000,
+    });
     yield put(loginGame({ userLogged_in }));
   } else {
+    swal("Wrong Password or Username!", {
+      title: "Ask your teacher",
+      icon: "warning",
+      closeOnEsc: false,
+    });
   }
 }
 export default function* rootSaga() {
